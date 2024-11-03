c1ad80acdb9388d7540bf7c994111b8d93af25a2
giorgi314 <giochakhnashvili@gmail.com>
2024-11-03 05:32:29 +0400

    made some changes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let burgerEl = document.getElementById('burgerbar');
 let stickyEl = document.getElementById('sticky');
 let numberEl = document.getElementById('number1');
 
+
 burgerEl.addEventListener('click', function(){
     navEl.classList.toggle('activenav')
     stickyEl.classList.toggle('activesticky')
@@ -10,6 +11,44 @@ burgerEl.addEventListener('click', function(){
 }
 )
 
+let allEl =document.getElementById('all');
+let saleEl = document.getElementById('sale');
+let rentEl = document.getElementById('rent');
+let saleBox1 = document.getElementById('sale1');
+let saleBox2 = document.getElementById('sale2');
+let saleBox3 = document.getElementById('sale3');
+let saleBox4 = document.getElementById('sale4');
+let rentBox1 = document.getElementById('rent1');
+let rentBox2 = document.getElementById('rent2');
+
+saleEl.addEventListener('click', function(){
+  rentBox1.classList.toggle('hiderent')
+  rentBox2.classList.toggle('hiderent')
+  saleBox1.classList.toggle('salewidth')
+  saleBox2.classList.toggle('salewidth')
+  saleBox3.classList.toggle('salewidth')
+  saleBox4.classList.toggle('salewidth')
+}
+)
+rentEl.addEventListener('click', function(){
+  saleBox1.classList.toggle('hidesale')
+  saleBox2.classList.toggle('hidesale')
+  saleBox3.classList.toggle('hidesale')
+  saleBox4.classList.toggle('hidesale')
+  rentBox1.classList.toggle('salewidth')
+  rentBox2.classList.toggle('salewidth')
+}
+)
+
+let subEl = document.getElementById('sub7');
+subEl.addEventListener('click', function() {
+  alert('Thank you! Our manager will contact you shortly')
+})
+
+let searchEl = document.getElementById('city-search')
+searchEl.addEventListener('click', function(){
+  alert('sorry, we dont have apartments in this city')
+})
 
 $(function(){
     $('.multiple-items').slick({
@@ -36,3 +75,4 @@ $(function(){
      ]
    });    
  });
+
